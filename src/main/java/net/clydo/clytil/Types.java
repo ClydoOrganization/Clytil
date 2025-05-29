f7cb59157c6c667f0ad076f71662799d67bfc355
RezaNajafian <reza.najafian.rena@gmail.com>
2025-05-29 21:44:43 +0330

    Added common types

--- src/main/java/net/clydo/clytil/Types.java
+++ src/main/java/net/clydo/clytil/Types.java
@@ -1,9 +1,65 @@
 package net.clydo.clytil;
 
 import lombok.experimental.UtilityClass;
+import org.jetbrains.annotations.NotNull;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Member;
+import java.lang.reflect.Method;
+import java.util.List;
+import java.util.stream.Stream;
 
 @UtilityClass
 public class Types {
+
+    public static final List<Class<?>> BOOLEAN = List.of(Boolean.class, boolean.class);
+    public static final List<Class<?>> BYTE = List.of(Byte.class, byte.class);
+    public static final List<Class<?>> SHORT = List.of(Short.class, short.class);
+    public static final List<Class<?>> CHAR = List.of(Character.class, char.class);
+    public static final List<Class<?>> INT = List.of(Integer.class, int.class);
+    public static final List<Class<?>> FLOAT = List.of(Float.class, float.class);
+    public static final List<Class<?>> LONG = List.of(Long.class, long.class);
+    public static final List<Class<?>> DOUBLE = List.of(Double.class, double.class);
+    public static final List<Class<?>> STRING = List.of(String.class);
+    public static final List<Class<?>> VOID = List.of(Void.class, void.class);
+
+    public static final List<Class<?>> NUMERICS = Inits.of(() ->
+            List.copyOf(
+                    Stream.of(BYTE, SHORT, INT, FLOAT, LONG, DOUBLE)
+                            .flatMap(List::stream)
+                            .toList()
+            )
+    );
+
+    public static final List<Class<?>> INTEGERS = Inits.of(() ->
+            List.copyOf(
+                    Stream.of(BYTE, SHORT, INT, LONG)
+                            .flatMap(List::stream)
+                            .toList()
+            )
+    );
+
+    public static final List<Class<?>> FLOATS = Inits.of(() ->
+            List.copyOf(
+                    Stream.of(FLOAT, DOUBLE)
+                            .flatMap(List::stream)
+                            .toList()
+            )
+    );
+
+    public Class<?> getValueType(@NotNull final Member member) {
+        Validates.requireParam(member, "member");
+
+        if (member instanceof Field field) {
+            return field.getType();
+        } else if (member instanceof Method method) {
+            return method.getReturnType();
+        }
+
+        throw new IllegalArgumentException(
+                "Unsupported member type: " + member.getClass().getName() + ". Expected Field or Method."
+        );
+    }
 
     public <T> boolean is(Class<T> type, Class<?> expected) {
         return type == expected;
