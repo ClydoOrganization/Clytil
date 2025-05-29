585c93a82d221e38f9a931bb9f735a1af22474a7
RezaNajafian <reza.najafian.rena@gmail.com>
2025-05-29 23:52:20 +0330

    Updated Copyright

--- src/main/java/net/clydo/clytil/iface/value/ValueExt.java
+++ src/main/java/net/clydo/clytil/iface/value/ValueExt.java
@@ -1,18 +1,18 @@
 /*
- * This file is part of Clyfigo.
+ * This file is part of Clytil.
  *
- * Clyfigo is free software: you can redistribute it and/or modify
+ * Clytil is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or (at your
  * option) any later version.
  *
- * Clyfigo is distributed in the hope that it will be useful,
+ * Clytil is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Clyfigo. If not, see
+ * along with Clytil. If not, see
  * <http://www.gnu.org/licenses/>.
  *
  * Copyright (C) 2024-2025 ClydoNetwork
