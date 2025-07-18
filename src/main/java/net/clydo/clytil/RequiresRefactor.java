0474b52f776b67c1dcff132fb2ea13500052794a
RezaNajafian <reza.najafian.rena@gmail.com>
2025-07-18 17:50:55 +0330

    feat: add RequiresRefactor annotation
    Introduces the @RequiresRefactor annotation for marking code elements that need refactoring. The annotation is retained in source and supports an optional value for additional context.

---
+++ src/main/java/net/clydo/clytil/RequiresRefactor.java
@@ -0,0 +1,47 @@
+/*
+ * This file is part of Clytil.
+ *
+ * Clytil is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * Clytil is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Clytil. If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * Copyright (C) 2025 ClydoNetwork
+ */
+
+package net.clydo.clytil;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.SOURCE)
+@Target({
+        ElementType.TYPE,
+        ElementType.FIELD,
+        ElementType.METHOD,
+        ElementType.PARAMETER,
+        ElementType.CONSTRUCTOR,
+        ElementType.LOCAL_VARIABLE,
+        ElementType.ANNOTATION_TYPE,
+        ElementType.PACKAGE,
+        ElementType.TYPE_PARAMETER,
+        ElementType.TYPE_USE,
+        ElementType.MODULE,
+        ElementType.RECORD_COMPONENT
+})
+public @interface RequiresRefactor {
+
+    String value() default "";
+
+}
